5c21b850d4e595e3bf9050709c7b9833348129cf
VickyYan0822 <143618285+VickyYan0822@users.noreply.github.com>
2023-11-13 11:14:23 -0500

    Initial commit

---
+++ part1.py
@@ -0,0 +1,4 @@
+#  takes an address or place name and returns the closest MBTA stop and other information about that MBTA stop. 
+import mbta_helper
+print(mbta_helper.find_stop_near("Boston Common"))
+
